b8dd1750f618aff180e338dc156fec6339cb038b
Akila Darshana <akiladarshana@gmail.com>
2012-04-12 15:44:38 +0530

    Added vertical tab host in landscape mode. Fixed keeping tab selection while orientation change
    Signed-off-by: Akila Darshana <akiladarshana@gmail.com>

--- src/gov/in/bloomington/open311/MainActivity.java
+++ src/gov/in/bloomington/open311/MainActivity.java
@@ -5,14 +5,27 @@
  */
 package gov.in.bloomington.open311;
 
+
+
+
+
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MainActivity extends TabActivity {
 
@@ -22,28 +35,39 @@ public class MainActivity extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        final TabHost tabHost = getTabHost();
+        tabHost.setup();
+
         Resources res = getResources();
-        TabHost tabHost = getTabHost();
+        Configuration cfg = res.getConfiguration();
+        boolean hor = cfg.orientation == Configuration.ORIENTATION_LANDSCAPE;
+
+        if (hor) {
+            TabWidget tw = tabHost.getTabWidget();
+            tw.setOrientation(LinearLayout.VERTICAL);
+        }
+        
+        
         TabHost.TabSpec spec;
         Intent intent;
         
         intent = new Intent().setClass(this, HomeActivity.class);
-        spec = tabHost.newTabSpec("home").setIndicator(res.getString(R.string.home), res.getDrawable(R.drawable.ic_menu_home)).setContent(intent);
+        spec = tabHost.newTabSpec("home").setIndicator(createIndicatorView(tabHost,res.getString(R.string.home), res.getDrawable(R.drawable.ic_menu_home))).setContent(intent);
         tabHost.addTab(spec);
         
         intent = new Intent().setClass(this, ReportActivity.class);
-        spec = tabHost.newTabSpec("report").setIndicator(res.getString(R.string.report), res.getDrawable(R.drawable.ic_menu_notifications)).setContent(intent);
+        spec = tabHost.newTabSpec("report").setIndicator(createIndicatorView(tabHost,res.getString(R.string.report), res.getDrawable(R.drawable.ic_menu_notifications))).setContent(intent);
         tabHost.addTab(spec);
         
         intent = new Intent().setClass(this, MyReportsActivity.class);
-        spec = tabHost.newTabSpec("my_reports").setIndicator(res.getString(R.string.my_reports), res.getDrawable(R.drawable.ic_menu_home)).setContent(intent);
+        spec = tabHost.newTabSpec("my_reports").setIndicator(createIndicatorView(tabHost,res.getString(R.string.my_reports), res.getDrawable(R.drawable.ic_menu_home))).setContent(intent);
         tabHost.addTab(spec);
         
         intent = new Intent().setClass(this, MyServersActivity.class);
-        spec = tabHost.newTabSpec("my_servers").setIndicator(res.getString(R.string.my_servers), res.getDrawable(R.drawable.ic_menu_star)).setContent(intent);
+        spec = tabHost.newTabSpec("my_servers").setIndicator(createIndicatorView(tabHost,res.getString(R.string.my_servers), res.getDrawable(R.drawable.ic_menu_star))).setContent(intent);
         tabHost.addTab(spec);
         
-        tabHost.setCurrentTab(0);
+        
     }
 
     @Override
@@ -62,5 +86,25 @@ public class MainActivity extends TabActivity {
     		break;
     	}
     	return true;
+    }
+    
+    
+    
+    //creating custom indicator view for use with horizontal configurations of the tab host
+    private View createIndicatorView(TabHost tabHost, CharSequence label, Drawable icon) {
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View tabIndicator = inflater.inflate(R.layout.tab_indicator,
+                tabHost.getTabWidget(), // tab widget is the parent
+                false); // no inflate params
+
+        final TextView tv = (TextView) tabIndicator.findViewById(R.id.title);
+        tv.setText(label);
+
+        final ImageView iconView = (ImageView) tabIndicator.findViewById(R.id.icon);
+        iconView.setImageDrawable(icon);
+
+        return tabIndicator;
     }
 }
